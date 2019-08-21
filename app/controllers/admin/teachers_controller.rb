4f97c415ecf4600a7a3e3e137065eeb4cf81c608
bolistovskiyroman@gmail.com <RemkA1980>
2019-08-21 15:11:22 +0300

    create view create and same style

--- app/controllers/admin/teachers_controller.rb
+++ app/controllers/admin/teachers_controller.rb
@@ -9,7 +9,15 @@ class Admin::TeachersController < Admin::BaseController
     @teacher = Teacher.new
   end
 
-  def create;
+  def create
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save
+      redirect_to admin_teachers_path, notice: 'Викладач успішно сворений'
+    else
+      flash_now[:alert] = 'Не вдалося успішно створити викладача'
+      render :new
+    end
   end
 
   def edit;
@@ -26,4 +34,9 @@ class Admin::TeachersController < Admin::BaseController
   def set_active_main_menu_item
     @main_menu[:teachers][:active] = true
   end
+
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :last_name, :description)
+  end
+
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Teacher < ApplicationRecord
+  validates :first_name, :last_name, :description, presence: true
 end
